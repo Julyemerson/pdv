bafea544d3c3366cdbc37b4ddb432e0e04bf39b0
Julyemerson Leonizio <jb.leonizio@gmail.com>
2024-10-03 23:55:35 -0300

    feat: start implementation of authetication

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { PrismaService } from './database/prisma.service';
 import { ClientsModule } from './clients/clients.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule, ClientsModule],
+  imports: [UsersModule, ProductsModule, OrdersModule, ClientsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
